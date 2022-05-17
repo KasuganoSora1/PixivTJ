358ac5e172a6d38a7d5852b64f5855485e7be1b8
spectre <zhang192831@gmail.com>
2022-05-17 20:00:15 +0800

    多线程完成，下一步计划，更新likecount,bookmarkcount

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import test
+from concurrent.futures import ThreadPoolExecutor,wait
+import web
+pool=ThreadPoolExecutor(10)
+for i in range(1,100000):
+        all_task=[pool.submit(web.write_from_page_id,str(k)) for k in range((i-1)*1000+1,i*1000)]
+        print("put to "+str((i-1)*1000+1)+","+str(i*1000)+" pool")
+        wait(all_task)
